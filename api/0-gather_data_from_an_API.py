d89ec7851d15f97443af6a89b95649749b99ce44
NK-Adeodatus <a.nkundiman@alustudent.com>
2025-02-25 19:00:21 +0200

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,44 +1,18 @@
-#!/usr/bin/python3
-"""
-Request from API; Return TODO list progress given employee ID
-"""
 import requests
-from sys import argv
 
-if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        exit(1)
+USER_URL = 'https://jsonplaceholder.typicode.com/users'
+TODO_URL = 'https://jsonplaceholder.typicode.com/todos'
 
-    employee_id = int(argv[1])
-    base_url = "https://jsonplaceholder.typicode.com"
+response = requests.get(USER_URL)
 
-    try:
-        user_response = requests.get(f"{base_url}/users/{employee_id}")
-        user_response.raise_for_status()  # Check for HTTP errors
-        user_data = user_response.json()
-        employee_name = user_data.get("name")  # Use .get()
+def extract_data(user_id):
+    employee = requests.get(f"{USER_URL}/{user_id}")
+    employee_todos = requests.get(f"{TODO_URLT}?userId=T{user_id}")
+    completed_tasks = []
+    employee_name = employee["name"]
 
-        todos_response = requests.get(f"{base_url}/todos?userId={employee_id}") # Filter tasks by userId
-        todos_response.raise_for_status()
-        todos_data = todos_response.json()
+    for todo in employee_todos:
+        if todo["completed"]:
+            completed_tasks.append(todo)
 
-        completed_tasks = [
-            task for task in todos_data if task.get("completed")
-        ]  # List comprehension for completed tasks
-        num_completed = len(completed_tasks)
-        total_tasks = len(todos_data)
-
-        print(
-            f"Employee {employee_name} is done with tasks({num_completed}/{total_tasks}):"
-        )
-
-        for task in completed_tasks:
-            print(f"\t {task.get('title')}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        exit(1)
-    except (ValueError, KeyError) as e:
-        print(f"Error: {e}")
-        exit(1)
+    print(f"Employee {employee_name} is done with task({len(completed_tasks)} {len(employee_name)})")
