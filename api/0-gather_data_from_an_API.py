3a1403da67daa23b791f1cb1d6b0cd13eee5ed71
NK-Adeodatus <a.nkundiman@alustudent.com>
2025-02-26 20:10:46 +0200

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
-"""
-Request from API; Return TODO list progress given employee ID
-"""
+'''
+gather employee data from API
+'''
+
+import re
 import requests
+import sys
 
+REST_API = "https://jsonplaceholder.typicode.com"
 
-USER_URL = 'https://jsonplaceholder.typicode.com/users'
-TODO_URL = 'https://jsonplaceholder.typicode.com/todos'
-
-response = requests.get(USER_URL)
-
-
-def extract_data(user_id):
-    employee = requests.get(f"{USER_URL}/{user_id}")
-    employee_todos = requests.get(f"{TODO_URLT}?userId=T{user_id}")
-    completed_tasks = []
-    employee_name = employee["name"]
-
-    for todo in employee_todos:
-        if todo["completed"]:
-            completed_tasks.append(todo)
-
-    done_tasks = len(completed_tasks)
-    print("Employee {} is done with tasks({}/{}):".format(employee_name,
-                                                          completed_tasks,
-                                                          done_tasks))
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            req = requests.get('{}/users/{}'.format(REST_API, id)).json()
+            task_req = requests.get('{}/todos'.format(REST_API)).json()
+            emp_name = req.get('name')
+            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
+            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    emp_name,
+                    len(completed_tasks),
+                    len(tasks)
+                )
+            )
+            if len(completed_tasks) > 0:
+                for task in completed_tasks:
+                    print('\t {}'.format(task.get('title')))
